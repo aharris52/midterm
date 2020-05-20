63c6500297a142affac716212d2de65ef28a8808
aharris52 <aharris@mail.greenriver.edu>
2020-05-20 18:35:08 -0400

    added views, index.php and composer

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+//Start a session
+session_start();
+
+// create an instance of the base class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function(){
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
