73c6fea5560976657d11ad470723c422a1a63a34
aharris52 <aharris@mail.greenriver.edu>
2020-05-20 20:42:53 -0400

    last commit, right after the clock

--- index.php
+++ index.php
@@ -16,9 +16,27 @@ $f3 = Base::instance();
 // define a default route
 $f3->route('GET /', function(){
 
-    echo '<h1>Welcome to my Food Page</h1>';
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    //echo '<h1>Welcome to my Midterm Page</h1>';
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+// survey route
+$f3->route('GET|POST /survey', function($f3){
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
+
+});
+
+// summary route
+$f3->route('GET|POST /summary', function($f3){
+
+    //echo '<h1>Welcome to my Midterm Page</h1>';
+    $view = new Template();
+    echo $view->render('views/summary.html');
+    var_dump($_POST);
 });
 
 // run fat free
---
+++ model/data-layer.php
@@ -0,0 +1,8 @@
+<?php
+
+    function getSurvey()
+    {
+        $survey = array("I love this crap!", "Save me Ricky Bobby! Save me Tom Cruise! Save me Oprah Winfrey!",
+                        "meh...");
+        return $survey;
+    }
